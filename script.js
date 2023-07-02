a6cbac8f98ba50910ea70c303e4cc6b05caa4ace
Mba Thomas <angelfab631@gmail.com>
2023-07-02 18:08:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -75,8 +75,8 @@ function updateValues() {
         .toFixed(2);
 
     const expense = (amounts
-            .filter(item => item < 0)
-            .reduce((acc, item) => (acc += item), 0) * -1)
+        .filter(item => item < 0)
+        .reduce((acc, item) => (acc += item), 0) * -1)
         .toFixed(2);
 
     balance.innerText = `$${total}`;
